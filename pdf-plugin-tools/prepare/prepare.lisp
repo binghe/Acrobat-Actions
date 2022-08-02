9170be5e13e2c0d1f388f92d3e0204dd3551e08c
Chun Tian (binghe) <binghe.lisp@gmail.com>
2022-08-02 13:42:09 +0200

    Stage work parsing SDK headers

--- pdf-plugin-tools/prepare/prepare.lisp
+++ pdf-plugin-tools/prepare/prepare.lisp
@@ -33,18 +33,60 @@
 checks for enums, structs or function prototypes and writes the
 corresponding C code to *STANDARD-OUTPUT*."
   (dolist (name *header-file-names*)
-    (let ((header-file (make-pathname :name name
-                                      :type "h"
+    (let ((header-file (make-pathname :name name :type "h"
                                       :defaults *sdk-extern-location*))
           (file-string (make-array '(0) :element-type 'simple-char
                                    :fill-pointer 0 :adjustable t)))
+      (format t ";; #include <~A.h>~%" name)
+      (format t "#|~%")
+      (setq *function-counter* 1)
       (with-output-to-string (out file-string)
         (with-open-file (in header-file)
-          (loop with if-context = nil
+          ;; NOTE: there are several #if contexts to detect: #if DEBUG, or ACROBAT_LIBRARY.
+          ;; when we met such a #if, we push it into POS-CONTEXTS as "DEBUG" or "ACROBAT_LIBRARY",
+          ;; which means that the following lines must be ignored. And when we are in such a
+          ;; context and meet "#else", the special-context is over and the next lines are
+          ;; needed.  When meeting "#endif", we do nothing since contexts do not nest so far.
+          ;; This should be enough for now.
+          (loop with contexts = '(nil)     ; the polarity of the current #if (T or NIL)
+                with pos-contexts = '(nil) ; the current if context when polarity is T
+                with neg-contexts = '(nil) ; the current if context when polarity is NIL
                 for line = (read-line in nil nil)
-                while line
-                do (handle-typedef line)
-                   (handle-function line)))))))
+                while line do ; this fails only when input file ends
+            (cond ((scan "#if\\s+(.*)" line)
+                   (register-groups-bind (neg-p context) ("#if\\s+(!)?(.*)" line)
+                     (cond (neg-p
+                            (push context neg-contexts)
+                            (push nil contexts))
+                           (t
+                            (push context pos-contexts)
+                            (push t contexts)))))
+                  ((scan "#else" line)
+                   (cond ((pop contexts)
+                          (push (pop pos-contexts) neg-contexts)
+                          (push nil contexts))
+                         (t
+                          (push (pop neg-contexts) pos-contexts)
+                          (push t contexts))))
+                  ((scan "#endif" line)
+                   (cond ((pop contexts)
+                          (pop pos-contexts))
+                         (t
+                          (pop neg-contexts))))
+                  ;; Here the tests must be exhaustive: for each contexts only one line is
+                  ;; preserved.
+                  ((or (member "DEBUG"           pos-contexts :test 'equal)
+                       (member "ACROBAT_LIBRARY" pos-contexts :test 'equal))
+                   nil) ; skip this line
+                  (t
+                   (format out "~A~%" line)))))) ; collect this line
+      ;; now the file-string can be safely parsed in multiple lines
+      (do-register-groups (body)
+          ("(?m)^(\\w*PROC\\([\\w\\s\\*,]+\\([\\w\\s\\*,]+\\)(,\\s*\\w+)?\\))" file-string)
+        (format t "~A: ~A~%" *function-counter* body)
+        (incf *function-counter*))
+      (format t "|#~%")
+      (terpri))))
 
 (defun prepare ()
   "Creates the missing file `fli.lisp' for PDF-PLUGIN-TOOLS from
@@ -61,9 +103,9 @@ the C header files of Acrobat Pro."
       (let ((*package* (find-package :pdf-plugin-tools))
             (*print-case* :downcase))
         (format t ";;; This file was generated automatically from Acrobat Pro's SDK headers.")
-        (terpri)        
-        (print '(in-package :pdf-plugin-tools))
         (terpri)
+        (print '(in-package :pdf-plugin-tools))
+        (terpri) (terpri)
         ;; let this function do all the work
         (parse-header-files))))
   :done)
