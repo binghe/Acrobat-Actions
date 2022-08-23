221bec22b100dcda3182089a6df2579176683248
Chun Tian (binghe) <binghe.lisp@gmail.com>
2022-08-23 10:14:04 +0200

    No need to handle comments in many other cases

--- pdf-plugin-tools/prepare/prepare.lisp
+++ pdf-plugin-tools/prepare/prepare.lisp
@@ -93,6 +93,7 @@ defintion and writes it to the output stream."
                  `(:variadic-num-of-fixed ,variadic-num-of-fixed))
              :calling-convention :cdecl)))
 
+;; https://blog.ostermiller.org/finding-comments-in-source-code-using-regular-expressions/
 (defparameter *remove-c-comment-regex*
   (create-scanner
    "(/\\*(?:[^*]|[\\r\\n]|(?:\\*+(?:[^*/]|[\\r\\n])))*\\*+/)"))
@@ -291,19 +292,12 @@ EXPORT statement."
     (when type-name
       (pprint `(fli:define-c-typedef ,(mangle-name type-name) :int)))))
 
-;; NOTE: the regex string /\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/ for matching C
-;; comments is learnt from (non-greedy matching doesn't work)
-;; https://blog.ostermiller.org/finding-comments-in-source-code-using-regular-expressions/
-;;
-;; /* comment1 */ type [*]var1, [*]var2; // comment2
+;; type [*]var1, [*]var2;
 (defparameter *type-and-names-regex*
   (create-scanner
    (concatenate 'string
                 "(?sm)\\s*"
-                "(?:/\\*(?:[^*]|[\\r\\n]|(?:\\*+(?:[^*/]|[\\r\\n])))*\\*+/\\s*)*" ; C comments
-                "(?://[^\\r\\n]*)?"     ; C++ comments (optional)
                 "([^/;,*]+)(?<!\\s)(?:(\\s*\\*\\s+|\\s+\\*\\s*)|\\s+)([\\w\\s,\\[\\]]+)\\s*;"
-                "\\s*(?://[^\\r\\n]*)?" ; C++ comments (optional)
                 )))
 
 (defun handle-struct-body (class body typedef-name &optional
@@ -404,7 +398,6 @@ corresponding FLI:DEFINE-C-STRUCT definition."
                 "(?sm)\\s*"
                 "typedef\\s+struct\\s+(\\w+)(?<!\\s)\\s*"
                 "\\*(\\w+),\\s*"
-                "(?:/\\*(?:[^*]|[\\r\\n]|(?:\\*+(?:[^*/]|[\\r\\n])))*\\*+/\\s*)?" ; C comments (optional)
                 "\\*(\\w+),\\s*"
                 "\\*(\\w+),\\s*"
                 "\\*(\\w+);"
